3974b61222e1273aa8c9ba81769c57f7e736707c
Filipe Feier <filipe_feier@hotmail.com>
2019-08-13 19:46:27 -0300

    Criação da primeira classe: Tabuleiro

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XadrezConsole.Tabuleiro;
 
 namespace XadrezConsole
 {
@@ -10,12 +11,10 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Posicao p = new Posicao(3,4);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Posicao " + p);
+            Console.ReadLine();
         }
     }
 }
